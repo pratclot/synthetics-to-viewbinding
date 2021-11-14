71d97b4e5d7a4336126f53b03d49b3ab00fc89d1
GitHub Action <action@github.com>
2021-11-14 17:26:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pratclot/syntheticstoviewbinding/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pratclot.syntheticstoviewbinding.services
+
+import com.github.pratclot.syntheticstoviewbinding.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/pratclot/syntheticstoviewbinding/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pratclot/syntheticstoviewbinding/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pratclot.syntheticstoviewbinding.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pratclot.syntheticstoviewbinding.MyBundle
 
 class MyProjectService(project: Project) {
 
